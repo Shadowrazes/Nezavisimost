1d0b7ab9b7076496df0edba19341940694706cae
Shadowrazes <hugehug@mail.ru>
2024-02-26 16:32:50 +0700

    add post template

--- src/js/app.js
+++ src/js/app.js
@@ -12,7 +12,7 @@ function isWebp() {
 
         webP.onload = webP.onerror = () => callback(webP.height === 2);
         webP.src =
-        'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+            'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
     };
 
     // Добавление класса _webp или _no-webp для HTML
@@ -26,84 +26,84 @@ function isWebp() {
 isWebp();
 
 const InsertPostContents = () => {
-	const headers = [];
-	const indexes = [0];
-	const articleContent = document.querySelector('.post__content');
-	// функция для получения предыдущего header
-	const getPrevHeader = (diff = 0) => {
-	  if ((indexes.length - diff) === 0) {
-		return null;
-	  }
-	  let header = headers[indexes[0]];
-	  for (let i = 1, length = indexes.length - diff; i < length; i++) {
-		header = header.contains[indexes[i]];
-	  }
-	  return header;
-	}
-	// функция для добавления item в headers
-	const addItemToHeaders = (el, diff) => {
-	  let header = headers;
-	  if (diff === 0) {
-		header = indexes.length > 1 ? getPrevHeader(1).contains : header;
-		indexes.length > 1 ? indexes[indexes.length - 1]++ : indexes[0]++;
-	  } else if (diff > 0) {
-		header = getPrevHeader().contains;
-		indexes.push(0);
-	  } else if (diff < 0) {
-		const parentHeader = getPrevHeader(Math.abs(diff) + 1);
-		for (let i = 0; i < Math.abs(diff); i++) {
-		  indexes.pop();
-		}
-		header = parentHeader ? parentHeader.contains : header;
-		parentHeader ? indexes[indexes.length - 1]++ : indexes[0]++;
-	  }
-	  header.push({ el, contains: [] });
-	}
-	// сформируем оглавление страницы для вставки его на страницу
-	let html = '';
-	const createTableOfContents = (items) => {
-	  html += '<ol>';
-	  for (let i = 0, length = items.length; i < length; i++) {
-		const url = `${location.href.split('#')[0]}#${items[i].el.id}`;
-		html += `<li><a href="${url}">${items[i].el.textContent}</a>`;
-		if (items[i].contains.length) {
-		  createTableOfContents(items[i].contains);
-		}
-		html += '</li>';
-	  }
-	  html += '</ol>';
-	}
+    const headers = [];
+    const indexes = [0];
+    const articleContent = document.querySelector('.post__header');
+    // функция для получения предыдущего header
+    const getPrevHeader = (diff = 0) => {
+        if ((indexes.length - diff) === 0) {
+            return null;
+        }
+        let header = headers[indexes[0]];
+        for (let i = 1, length = indexes.length - diff; i < length; i++) {
+            header = header.contains[indexes[i]];
+        }
+        return header;
+    }
+    // функция для добавления item в headers
+    const addItemToHeaders = (el, diff) => {
+        let header = headers;
+        if (diff === 0) {
+            header = indexes.length > 1 ? getPrevHeader(1).contains : header;
+            indexes.length > 1 ? indexes[indexes.length - 1]++ : indexes[0]++;
+        } else if (diff > 0) {
+            header = getPrevHeader().contains;
+            indexes.push(0);
+        } else if (diff < 0) {
+            const parentHeader = getPrevHeader(Math.abs(diff) + 1);
+            for (let i = 0; i < Math.abs(diff); i++) {
+                indexes.pop();
+            }
+            header = parentHeader ? parentHeader.contains : header;
+            parentHeader ? indexes[indexes.length - 1]++ : indexes[0]++;
+        }
+        header.push({ el, contains: [] });
+    }
+    // сформируем оглавление страницы для вставки его на страницу
+    let html = '';
+    const createTableOfContents = (items) => {
+        html += '<ol>';
+        for (let i = 0, length = items.length; i < length; i++) {
+            const url = `${location.href.split('#')[0]}#${items[i].el.id}`;
+            html += `<li><a href="${url}">${items[i].el.textContent}</a>`;
+            if (items[i].contains.length) {
+                createTableOfContents(items[i].contains);
+            }
+            html += '</li>';
+        }
+        html += '</ol>';
+    }
 
-	if(articleContent){
-	  const contentsList = document.querySelector('.post__contents-list');
-	  if(contentsList){
-		// добавим заголовки в headers
-		articleContent.querySelectorAll('h2, h3, h4').forEach((el, index) => {
-			if (!el.id) {
-			el.id = `id-${index}`;
-			}
-			if (!index) {
-			addItemToHeaders(el);
-			return;
-			}
-			const diff = el.tagName.substring(1) - getPrevHeader().el.tagName.substring(1);
-			addItemToHeaders(el, diff);
-		});
+    if (articleContent) {
+        const contentsList = document.querySelector('.post__header-contents-list');
+        if (contentsList) {
+            // добавим заголовки в headers
+            articleContent.querySelectorAll('h2, h3, h4').forEach((el, index) => {
+                if (!el.id) {
+                    el.id = `id-${index}`;
+                }
+                if (!index) {
+                    addItemToHeaders(el);
+                    return;
+                }
+                const diff = el.tagName.substring(1) - getPrevHeader().el.tagName.substring(1);
+                addItemToHeaders(el, diff);
+            });
 
-		createTableOfContents(headers);
-		contentsList.insertAdjacentHTML('afterbegin', html);
-	  }
-	}
+            createTableOfContents(headers);
+            contentsList.insertAdjacentHTML('afterbegin', html);
+        }
+    }
 }
 
-function CallbackFormInit(){
+function CallbackFormInit() {
     let forms = document.querySelectorAll('form');
 
-    if(forms.length > 0){
-        forms.forEach((form) =>{
+    if (forms.length > 0) {
+        forms.forEach((form) => {
             let phoneInputs = form.querySelectorAll('input[name="phone"]');
 
-            if(phoneInputs.length > 0) {
+            if (phoneInputs.length > 0) {
                 phoneInputs.forEach((phoneInput) => {
                     const phoneMask = new IMask(phoneInput, {
                         mask: "+{7} (000) 000-00-00",
@@ -111,10 +111,10 @@ function CallbackFormInit(){
 
                     phoneInput.addEventListener('input', (event) => {
                         event.preventDefault();
-                
+
                         if (!phoneMask.masked.isComplete) {
                             phoneInput.classList.add("uk-form-danger");
-                        } 
+                        }
                         else {
                             phoneInput.classList.remove("uk-form-danger");
                         }
@@ -122,34 +122,34 @@ function CallbackFormInit(){
 
                     form.addEventListener('submit', (event) => {
                         event.preventDefault();
-                
-                        if (!phoneMask.masked.isComplete){
+
+                        if (!phoneMask.masked.isComplete) {
                             return;
                         }
-						
-						let formData = {};
-						let inputs = form.querySelectorAll('input:not([type="submit"]), textarea');
-						if(inputs.length > 0) {
-							inputs.forEach((input) => {
-								formData[input.getAttribute('name')] = input.value;
-							})
-						}
+
+                        let formData = {};
+                        let inputs = form.querySelectorAll('input:not([type="submit"]), textarea');
+                        if (inputs.length > 0) {
+                            inputs.forEach((input) => {
+                                formData[input.getAttribute('name')] = input.value;
+                            })
+                        }
 
                         let successPopupNode = document.querySelector('#callback-popup__success');
-						UIkit.modal(successPopupNode).show();
+                        UIkit.modal(successPopupNode).show();
 
-						// jQuery.ajax({
-						// 	url: '/wp-admin/admin-ajax.php',
-						// 	method: 'post',
-						// 	data: {
-						// 		action: 'sendForm',
-						// 		data: JSON.stringify(formData)
-						// 	},
-						// 	success: function(data){
-						// 		let successPopupNode = document.querySelector('#success-popup');
-						// 		UIkit.modal(successPopupNode).show();
-						// 	}
-						// });
+                        // jQuery.ajax({
+                        // 	url: '/wp-admin/admin-ajax.php',
+                        // 	method: 'post',
+                        // 	data: {
+                        // 		action: 'sendForm',
+                        // 		data: JSON.stringify(formData)
+                        // 	},
+                        // 	success: function(data){
+                        // 		let successPopupNode = document.querySelector('#success-popup');
+                        // 		UIkit.modal(successPopupNode).show();
+                        // 	}
+                        // });
                     })
                 })
             }
@@ -157,30 +157,30 @@ function CallbackFormInit(){
     };
 }
 
-function LoadMapOnScroll(){
+function LoadMapOnScroll() {
     let isMapAppend = false;
-	let mapNode = document.querySelector('.map');
-	if(mapNode) {
-		document.addEventListener('scroll', (event) => {
-			if(!isMapAppend) {
-				if(window.scrollY > 1000) {
-					let script = document.createElement('script');
-					
-					script.src = 'https://nalogsib.ru/wp-content/themes/NalogSib/js/map.js';
-					script.type = 'text/javascript';
-					
-					mapNode.append(script);
-					isMapAppend = true;
-				}
-			}
-		});
-	}
+    let mapNode = document.querySelector('.map');
+    if (mapNode) {
+        document.addEventListener('scroll', (event) => {
+            if (!isMapAppend) {
+                if (window.scrollY > 1000) {
+                    let script = document.createElement('script');
+
+                    script.src = 'https://nalogsib.ru/wp-content/themes/NalogSib/js/map.js';
+                    script.type = 'text/javascript';
+
+                    mapNode.append(script);
+                    isMapAppend = true;
+                }
+            }
+        });
+    }
 }
 
-function InitCenteredSliders(){
+function InitCenteredSliders() {
     let centeredSliders = document.querySelectorAll('.slider_centered');
-    if(centeredSliders.length > 0){
-        centeredSliders.forEach((centeredSlider) =>{
+    if (centeredSliders.length > 0) {
+        centeredSliders.forEach((centeredSlider) => {
             UIkit.slider(centeredSlider, {
                 center: true
             });
@@ -201,7 +201,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     // particlesJS.load('particles-slider', 'static/ParticlesJSON/GreenHexagons.json');
 
     // Содержание статьи по заголовкам
-    // InsertPostContents();
+    InsertPostContents();
 
     // Прогрузка карты при скролле
     // LoadMapOnScroll()
